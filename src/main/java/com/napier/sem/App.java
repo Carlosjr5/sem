7d43133e0c00accf07dfdd27d92edbb08834da27
Angel Tenev <angel_ntenev@yahoo.com>
2021-03-02 18:02:29 +0000

    Revamp 5 New driver manager attempt

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -28,7 +28,7 @@ public class App {
             {
                 // Wait a bit for db to start
                 Thread.sleep(30000);
-                // Connect to database
+                // Connect to databas
                 //con = DriverManager.getConnection("jdbc:mysql://db:3306/employees?useSSL=false", "root", "example");
                 con = DriverManager.getConnection("jdbc:mysql://localhost:33060/employees?useSSL=true", "root", "example");
                 System.out.println("Successfully connected");
