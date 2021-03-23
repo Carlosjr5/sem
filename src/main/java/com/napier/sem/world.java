dbf750a6bbd70a97e46998d68c23f44cbb87f93d
Carlosjr5 <cjimenezr23@gmail.com>
2021-03-23 17:30:51 +0000

    World Class

---
+++ src/main/java/com/napier/sem/world.java
@@ -0,0 +1,33 @@
+package com.napier.sem;
+
+
+public class world {
+
+        /**
+         * ID Number
+         */
+        public int ID;
+
+        /**
+         *  Name
+         */
+        public String Name;
+
+        /**
+         * Country Code
+         */
+        public String CountryCode;
+
+        /**
+         * Country District
+         */
+        public String District;
+
+        /**
+         * Employee's salary
+         */
+        public String Population;
+
+
+    }
+
