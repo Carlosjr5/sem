39f4d2a137e3754af48d45264a12ec50a5acc3ec
Veselin Zanev <veskozanev4@gmail.com>
2021-03-24 00:28:36 +0000

    Issue34 UnitTest attempt

---
+++ src/test/java/com/napier/sem/Issue34.java
@@ -0,0 +1,26 @@
+package com.napier.sem;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.util.ArrayList;
+
+public class Issue34 {
+    static App app;
+
+
+    @BeforeAll
+    static void init()
+    {
+        app = new App();
+    }
+
+    @Test
+    void printSalariesTestNull()
+    {
+        app.printSalaries(null);
+    }
+}
